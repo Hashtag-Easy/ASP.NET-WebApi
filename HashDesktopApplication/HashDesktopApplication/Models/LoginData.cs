70c6add42a01f2951b9b899540ef5dc9945bea0e
Kacper Żabiński <kacperzab0@wp.pl>
2022-02-25 21:09:47 +0100

    [0.1.0] Init Commit

---
+++ HashDesktopApplication/HashDesktopApplication/Models/LoginData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashDesktopApplication.Models
+{
+    public class LoginData
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public LoginData()
+        {
+            UserName = string.Empty;
+            Password = string.Empty;
+        }
+    }
+}
---
+++ HashDesktopApplication/HashSharedPackages/Services/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSharedPackages.Services
+{
+    public class Session
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
